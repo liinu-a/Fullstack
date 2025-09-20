67cff7c7e3a67100b0de25f18305d916358fc370
liinu-a <liinu.arola@helsinki.fi>
2025-09-20 23:40:14 +0300

    1.6: unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+const Button = ({onClick, rating}) => <button onClick={onClick}>{rating}</button>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +10,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} rating="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} rating="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} rating="bad"/>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
